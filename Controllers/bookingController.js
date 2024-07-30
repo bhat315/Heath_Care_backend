2dd313ed4d59e108f1e61bc7e899b45d2f22f198
bhat315 <157919481+bhat315@users.noreply.github.com>
2024-07-30 14:24:00 +0530

    Update bookingController.js

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -35,7 +35,8 @@ export const getCheckoutSession = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             mode: 'payment',
-            success_url: `${process.env.CLIENT_SITE_URL}/checkout-success`,
+            // success_url: `${process.env.CLIENT_SITE_URL}/checkout-success`,
+            success_url: `https://mix-frontend.onrender.com/checkout-success`,
             cancel_url: `${req.protocol}://${req.get('host')}/doctors/${doctor.id}`,
             customer_email: user.email,
             client_reference_id: req.params.doctorId,
